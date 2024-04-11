932e7d8d08fd43ee6d301f48184fb8ad2ddf7c32
uBusisiwe <156895076+uBusisiwe@users.noreply.github.com>
2024-04-11 10:02:59 +0200

    RestfulAPI

---
+++ OneDrive - Mutualism/Desktop/Projects/API-RESTFULAPI/main.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function createPostList() {
+    const postList = document.getElementById("postList");
+
+    postList.innerHTML="";
+
+     //*SEARCH POSTS*//
+    fetch ('https://dummyjson.com/posts/search?q=love')
+    .then(res => res.json())
+    .then(console.log);
+
+    //*GET ALL POSTS*//
+    fetch('https://dummyjson.com/posts')
+     .then(res => res.json())
+     .then(console.log);
+
+    //*GETS SINGLE POST*//
+    fetch('https://dummyjson.com/posts/1')
+     .then(res => res.json())
+     .then(console.log);
+
+     //*GET ALL POSTS USER ID
+     fetch('https://dummyjson.com/posts/user/5')
+     .then(res => res.json())
+     .then(console.log);
+            
+     //*GET POSTS COMMENTS*//
+     fetch('https://dummyjson.com/posts/1/comments')
+     .then(res => res.json())
+     .then(console.log);
+     
+     //*ADD A NEW POST*//
+     fetch('https://dummyjson.com/posts/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+      title: 'I am in love with someone.',
+      userId: 5,
+    /* other post data */
+  })
+})
+.then(res => res.json())
+.then(console.log);
+
+       //*UPDATE POST*//
+        fetch('https://dummyjson.com/posts/1', {
+         method: 'PUT', /* or PATCH */
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({
+         title: 'I think I should shift to the moon',
+     })
+   })
+      .then(res => res.json())
+      .then(console.log);
+
+   //*DELETE A POST*//
+        fetch('https://dummyjson.com/posts/1', {
+         method: 'DELETE',
+    })
+      .then(res => res.json())
+      .then(console.log);
+            
+
+            
+  
+    // Simulated data (replace with actual data from DMM API)
+    const posts = [
+      { id: 1, title: "Search posts", content: "https://dummyjson.com/posts/search?q=love"}, 
+      { id: 2, title: "Get all posts", content: "https://dummyjson.com/posts" },
+      { id: 3, title: "Get single post", content: "https://dummyjson.com/posts/1" },
+      { id: 4, title: "Get all post user id", content: "https://dummyjson.com/posts/user/5" },
+      { id: 5, title: "Get post comments", content: "https://dummyjson.com/posts/1/comments" },
+      { id: 6, title: "Add a new post", content: "https://dummyjson.com/posts/add" }
+    ];
+
+
+  
+    // Populate post list
+    posts.forEach(post => {
+      const listItem = document.createElement("post");
+      const link = document.createElement("https://dummyjson.com/docs/posts");
+      link.textContent = post.DummyJSON;
+      link.href = `Post ${post.id}`; // Navigate to post page with ID parameter
+      listItem.appendChild(link);
+      postList.appendChild(listItem);
+    });
+  });
+
+  window.onload = createPostList;
+
---
+++ OneDrive - Mutualism/Desktop/Projects/sample-vue-project/src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+
+let value = 45;
+
+const reactV = ref(45)
+
+const items = ['name 1',' name2']
+
+const changeNum=()=>{
+  reactV.value = 67;
+
+
+  console.log(value);
+}
+
+
+console.log(value);
+</script>
+
+<template>
+  <div>
+    <h1>{{ reactV }}</h1>
+
+
+    <button @click="changeNum">Change the number</button>
+
+  
+    <ul>
+      <li
+        v-for="(item,index) in items"
+        v-bind:key="index"
+      >
+        {{ item }}
+    </li>
+    </ul>
+
+    </div>
+  
+   <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+
+
+
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
